41ee945a6ad20d22963783f02a96d587f5f44ff2
Peter Arato <it.arato@gmail.com>
2025-10-18 16:41:15 -0400

    Shooting.

---
+++ src/bullet.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "asset_manager.h"
+#include "common.h"
+#include "raylib.h"
+#include "sprite_group.h"
+
+struct Bullet {
+ public:
+  Bullet(Vector2 const pos, int const pixel_size, float const speed, int const west_wall, int const east_wall)
+      : pos(pos), pixel_size(pixel_size), speed(speed), west_wall(west_wall), east_wall(east_wall) {
+    sprite_group.push_sprite(
+        Sprite{static_cast<float>(pixel_size), asset_manager.textures[TextureNames::BulletShort],
+               Vector2{static_cast<float>(asset_manager.textures[TextureNames::BulletShort]->width),
+                       static_cast<float>(asset_manager.textures[TextureNames::BulletShort]->height)},
+               1, 0});
+  }
+
+  void draw() const {
+    sprite_group.draw(pos);
+  }
+
+  void update() {
+    sprite_group.update();
+    pos.x += speed * GetFrameTime();
+  }
+
+  bool is_dead() const {
+    return pos.x < west_wall || pos.x > east_wall;
+  }
+
+ private:
+  Vector2 pos;
+  int const pixel_size;
+  float const speed;
+  SpriteGroup sprite_group{};
+  int const west_wall;
+  int const east_wall;
+};
